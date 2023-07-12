f6b8be271e1270e1a3e9bcb92980ee7168a5833b
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-12 17:25:04 +0530

    I completed AddTest

---
+++ src/com/improve10x/fibonacci/remainder/Add.java
@@ -0,0 +1,15 @@
+package com.improve10x.fibonacci.remainder;
+
+public class Add {
+    public static void main(String[] args) {
+        Add add = new Add();
+        boolean answer = add.sumIsLessThan100(5,5);
+        System.out.println(answer);
+    }
+    public boolean sumIsLessThan100(int i, int i1) {
+        if (i + i1<100){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/com/improve10x/fibonacci/remainder/AddTest.java
@@ -0,0 +1,20 @@
+package com.improve10x.fibonacci.remainder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddTest {
+
+    @Test
+    public void nothing(){
+
+    }
+
+    @Test
+    public void givenOneAndThree_returnRemainderOne(){
+        Add add = new Add();
+        boolean twoAndThree = add.sumIsLessThan100(2,3);
+        assertEquals(true,twoAndThree);
+    }
+}
