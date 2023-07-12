22a6177d74951d33cc0a1bc77f5a0a4d1a98a89f
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-12 20:39:16 +0530

    I added setup method

--- src/com/improve10x/fibonacci/add/AddTest.java
+++ src/com/improve10x/fibonacci/add/AddTest.java
@@ -1,10 +1,16 @@
 package com.improve10x.fibonacci.add;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddTest {
+    private Add add;
+    @BeforeEach
+    public void setup(){
+         add = new Add();
+    }
 
     @Test
     public void nothing(){
@@ -13,21 +19,18 @@ public class AddTest {
 
     @Test
     public void givenTwentyTwoAndFifteen_returnTrue(){
-        Add add = new Add();
         boolean twentyTwoAndFifteen = add.sumIsLessThan100(22,15);
         assertEquals(true,twentyTwoAndFifteen);
     }
 
     @Test
     public void givenEightyThreeAndThirtyFour_returnFalse(){
-        Add add = new Add();
         boolean eightyThreeAndThirtyFour = add.sumIsLessThan100(83,34);
         assertEquals(false,eightyThreeAndThirtyFour);
     }
 
     @Test
     public void givenThreeAndSeventySeven_returnF(){
-        Add add = new Add();
         boolean threeAndSeventySeven = add.sumIsLessThan100(3,77);
         assertEquals(true,threeAndSeventySeven);
     }
