670189bcb9212b785e22cb98af6b5cf6d2698c66
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-16 11:50:23 +0530

    I Completed  IntegerDivisibleByFive  for TDD

--- src/com/improve10x/fibonacci/integerdivisiblebyfive/IntegerDivisibleByFive.java
+++ src/com/improve10x/fibonacci/integerdivisiblebyfive/IntegerDivisibleByFive.java
@@ -2,10 +2,10 @@ package com.improve10x.fibonacci.integerdivisiblebyfive;
 
 public class IntegerDivisibleByFive {
     public boolean find(int a) {
-        boolean result = false;
         if (a % 5 == 0){
             return true;
+        }else{
+            return false;
         }
-        return result;
     }
 }
