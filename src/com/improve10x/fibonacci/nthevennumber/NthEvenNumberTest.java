17337f09cf2753914fc79da325d9d64dc35c004d
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-17 12:12:00 +0530

    I Completed Nth even number for  TDD

---
+++ src/com/improve10x/fibonacci/nthevennumber/NthEvenNumberTest.java
@@ -0,0 +1,57 @@
+package com.improve10x.fibonacci.nthevennumber;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NthEvenNumberTest {
+
+    private NthEvenNumber nthEvenNumber;
+
+    @BeforeEach
+    public void setup() {
+        nthEvenNumber = new NthEvenNumber();
+    }
+
+    @Test
+    public void nothing() {
+    }
+
+    @Test
+    public void givenEvenNumberIsTwo_returnEvenNumberIsTwo() {
+        int evenNumber = nthEvenNumber.findEvenNumber(2);
+        assertEquals(2, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsTen_returnEvenNumberIsEighteen() {
+        int evenNumber = nthEvenNumber.findEvenNumber(10);
+        assertEquals(18, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsSix_returnEvenNumberIsEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(6);
+        assertEquals(10, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsHundred_returnEvenNumberIsOneNinetyEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(100);
+        assertEquals(198, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsSixty_returnEvenNumberIsOneOneEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(60);
+        assertEquals(118, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsThousand_returnEvenNumberIsNineteenNinetyEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(1000);
+        assertEquals(1998, evenNumber);
+    }
+
+}
