820a595b7c013ba8c29f02d57862b44833119ddf
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-17 16:11:05 +0530

    I completed IntegersAreEqualTest

---
+++ src/com/improve10x/fibonacci/arethenumbersequal/IntegersAreEqual.java
@@ -0,0 +1,11 @@
+package com.improve10x.fibonacci.arethenumbersequal;
+
+public class IntegersAreEqual {
+    public boolean isEqual(int num1, int num2) {
+        if (num1 % num2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ src/com/improve10x/fibonacci/arethenumbersequal/IntegersAreEqualTest.java
@@ -0,0 +1,48 @@
+package com.improve10x.fibonacci.arethenumbersequal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegersAreEqualTest {
+
+    @Test
+    public  void nothing(){
+
+    }
+
+    @Test
+    public void givenMinusTen_returnTrue(){
+        IntegersAreEqual integersAreEqual = new IntegersAreEqual();
+        boolean minusTen =  integersAreEqual.isEqual(-10,-10);
+        assertEquals(true,minusTen);
+    }
+
+    @Test
+    public void givenMinusOne_returnTrue(){
+        IntegersAreEqual integersAreEqual = new IntegersAreEqual();
+        boolean minusOne =  integersAreEqual.isEqual(-1,-1);
+        assertEquals(true,minusOne);
+    }
+
+    @Test
+    public void givenOne_returnTrue(){
+        IntegersAreEqual integersAreEqual = new IntegersAreEqual();
+        boolean one =  integersAreEqual.isEqual(1,1);
+        assertEquals(true,one);
+    }
+
+    @Test
+    public void givenFiveAndSix_returnFalse(){
+        IntegersAreEqual integersAreEqual = new IntegersAreEqual();
+        boolean number =  integersAreEqual.isEqual(5,6);
+        assertEquals(false,number);
+    }
+
+    @Test
+    public void given36And35_returnFalse(){
+        IntegersAreEqual integersAreEqual = new IntegersAreEqual();
+        boolean minusTen =  integersAreEqual.isEqual(36,35);
+        assertEquals(false,minusTen);
+    }
+}
