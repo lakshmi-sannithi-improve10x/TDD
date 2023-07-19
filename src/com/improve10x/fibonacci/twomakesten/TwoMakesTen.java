2ab4c3d40ffc9b3b0896eb547e4c72cbd01a231b
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-19 10:25:57 +0530

    I modified code  Two makes ten

--- src/com/improve10x/fibonacci/twomakesten/TwoMakesTen.java
+++ src/com/improve10x/fibonacci/twomakesten/TwoMakesTen.java
@@ -3,9 +3,7 @@ package com.improve10x.fibonacci.twomakesten;
 public class TwoMakesTen {
     public boolean makesTen(int a, int b) {
         boolean result = false;
-        int num1 = a;
-        int num2 = b;
-        if (num1 == 10 || num2 == 10 || num1 + num2 == 10) {
+        if (a == 10 || b == 10 || a + b == 10) {
             result = true;
         }
         return result;
