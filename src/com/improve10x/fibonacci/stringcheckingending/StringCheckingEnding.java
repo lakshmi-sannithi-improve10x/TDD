e35fdf3620c56b424d1b38a929996c75e5e877c5
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-19 14:28:03 +0530

    I modified method name StringCheckingEnding.

--- src/com/improve10x/fibonacci/stringcheckingending/StringCheckingEnding.java
+++ src/com/improve10x/fibonacci/stringcheckingending/StringCheckingEnding.java
@@ -2,7 +2,7 @@ package com.improve10x.fibonacci.stringcheckingending;
 
 public class StringCheckingEnding {
 
-    public boolean findSuffix(String originalText, String suffix) {
+    public boolean checkingSuffix(String originalText, String suffix) {
         if (originalText == null) {
             return false;
         } else if (originalText.endsWith(suffix)) {
--- src/com/improve10x/fibonacci/stringcheckingending/StringCheckingEndingTest.java
+++ src/com/improve10x/fibonacci/stringcheckingending/StringCheckingEndingTest.java
@@ -18,43 +18,43 @@ public class StringCheckingEndingTest {
 
     @Test
     public  void givenNull_returnFalse(){
-        boolean text = stringCheckingEnding.findSuffix("null","nu");
+        boolean text = stringCheckingEnding.checkingSuffix("null","nu");
         assertEquals(false,text);
     }
     @Test
     public  void givenEmptyString_returnTrue(){
-        boolean text = stringCheckingEnding.findSuffix("","");
+        boolean text = stringCheckingEnding.checkingSuffix("","");
         assertEquals(true,text);
     }
     @Test
     public  void givenAbc_returnTrue(){
-        boolean text = stringCheckingEnding.findSuffix("abc","bc");
+        boolean text = stringCheckingEnding.checkingSuffix("abc","bc");
         assertEquals(true,text);
     }
 
     @Test
     public  void givenAbcAndD_returnTrue(){
-        boolean text = stringCheckingEnding.findSuffix("abc","d");
+        boolean text = stringCheckingEnding.checkingSuffix("abc","d");
         assertEquals(false,text);
     }
 
     @Test
     public void givenSamurai_returnFalse(){
-        boolean text = stringCheckingEnding.findSuffix("samurai","zi");
+        boolean text = stringCheckingEnding.checkingSuffix("samurai","zi");
         assertEquals(false,text);
     }
 
     @Test
     public void givenFeminine_returnTrue(){
         StringCheckingEnding stringCheckingEnding = new StringCheckingEnding();
-        boolean text = stringCheckingEnding.findSuffix("feminine","nine");
+        boolean text = stringCheckingEnding.checkingSuffix("feminine","nine");
         assertEquals(true,text);
     }
 
     @Test
     public void givenConvention_returnFalse(){
         StringCheckingEnding stringCheckingEnding = new StringCheckingEnding();
-        boolean text = stringCheckingEnding.findSuffix("Convention","tio");
+        boolean text = stringCheckingEnding.checkingSuffix("Convention","tio");
         assertEquals(false,text);
     }
 }
