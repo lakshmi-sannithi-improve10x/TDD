81af05866a91e6658a1cbd95d4a4c57d6474a0c6
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-24 14:07:58 +0530

    I completed PlayerTest and CoinTest with TDD approach.

---
+++ src/com/improve10x/fibonacci/templerungame/Coin.java
@@ -0,0 +1,19 @@
+package com.improve10x.fibonacci.templerungame;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        if (value < 0 || value > 100) {
+            throw new InvalidCoinValueException();
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public class InvalidCoinValueException extends RuntimeException {
+    }
+}
